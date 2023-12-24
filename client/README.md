307685a9b29a42be59eeeab6f909e423b22259fc
Kunal Kishor <kunalbth2002@gmail.com>
2023-12-24 20:44:30 +0530

    start

---
+++ client/README.md
@@ -0,0 +1,1 @@
+working on tenserflow.js
---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Object from './Object';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>hi kunal</h1>
+      <Object />
+    </div>
+  );
+}
+
+export default App;
